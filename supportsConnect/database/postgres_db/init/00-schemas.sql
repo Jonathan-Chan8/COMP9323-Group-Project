7dbfd652fb329e3d6ad357a8b3d27c4104ff0464
张玉轩 <770385958@qq.com>
2021-07-21 19:28:43 +0800

    1. table names are all plural and only the first letter is capitalized 2. use '_' to separate words in a table name 3. use 'camel case' in attribute names

--- supportsConnect/database/postgres_db/init/00-schemas.sql
+++ supportsConnect/database/postgres_db/init/00-schemas.sql
@@ -1,9 +1,13 @@
--- connect to the correct db 
+-- connect to the correct db
 -- this name is determined in the docker-compose file
 \connect supportsconnect_database
 
 -- ALTER USER test PASSWORD 'test';
 -- GRANT ALL PRIVILEGES ON DATABASE test TO test;
+
+-- table names are all plural and only the first letter is capitalized
+-- use '_' to separate words in a table name
+-- use 'camel case' in attribute names
 
 create domain EmailValue as
     varchar(100) check (value like '%@%.%');
@@ -13,57 +17,96 @@ create domain NameValue as varchar(20);
 create domain RepetitionType as varchar(10)
     check (value in ('daily','weekly','monthly'));
 
+create domain Description as varchar(50)
+
 create table Users (
     id                serial,
+    username          varchar(20) not null,
+    password          varchar(20) not null,
+    accountType       char(1) not null check (value in ('g', 'c', 'w'))  --guardian/client/worker
     email             EmailValue not null,
-    givenName         NameValue not null,
-    familyName        NameValue,
-    date_of_birth     date,
+    firstName         NameValue not null,
+    lastName          NameValue,
+    dateOfBirth       date,
+    age               integer,
+    gender            varchar(6) check (value in ('male','female','other')),
+    contactNo         varchar(15),
+    homeAddress       Description,
+    shortBio          varchar(140),
     primary key (id)
 );
 
-create table Recipients (
+create table Clients (
     id                integer references Users(id),
-    recipient_id      serial,
-    address           varchar(50),
+    asGuardian        boolean not null,
+    address           Description not null,
+    allergies         Description,
+    likes             Description,
+    dislikes          Description,
+    healthNeeds       Description,
     primary key (id)
 );
 
 create table Support_workers (
     id                  integer references Users(id),
-    support_worker_id   serial,
+    languages           Description,
+    interests           Description,
+    primary key (id)
+);
+
+create table Work_histories  (
+    id                serial,
+    worker            integer references Support_workers(id),
+    location          Description,
+    startDate         date,
+    endDate           date,
+    primary key (id)
+);
+
+create table Trainings (
+    id                serial,
+    worker            integer references Support_workers(id),
+    subject           NameValue,
+    institution       NameValue,
+    startDate         date,
+    endDate           date,
     primary key (id)
 );
 
 create table Connected_pairs (
     id                  serial,
-    support_worker_id   integer references Support_workers(id),
-    recipient_id        integer references Recipients(id),
-    date_connected      date,
+    supportWorkerId     integer references Support_workers(id),
+    clientId            integer references Clients(id),
+    dateConnected       date,
     primary key (id)
 );
 
-create table Appointments (
+create table Shifts (
     id                  serial,
-    connected_id        integer references Connected_pairs(id),
-    available           boolean not null default true,
-    start_time          timestamp not null,
-    end_time            timestamp,
+    connectedId         integer references Connected_pairs(id),
+    shiftStatus         boolean not null default true,
+    workerStatus        boolean not null default true,
+    clientStatus        boolean not null default true,
+    startTime           timestamp not null,
+    endTime             timestamp,
     duration            interval,
-    repetition          RepetitionType,
+    frequency           RepetitionType,
     primary key (id)
 );
 
-create table Schedules (
+create table Activities (
     id                  serial,
-    support_worker_id   integer references Support_workers(id),
-    available_time      timestamp,
+    shift               integer references Shifts(id),
+    location            Description,
     primary key (id)
 );
 
 create table Reports (
     id                  serial,
-    appointment_id      integer references Appointments(id),
-    report_content      text,
+    activity            integer references activity(id),
+    mood                varchar(10) not null check (value in ('Angry','Sad','Moderate','Happy','Hyperactive')),
+    incident            boolean default false,
+    incidentReport      text,
+    sessionReport       text,
     primary key (id)
 );
