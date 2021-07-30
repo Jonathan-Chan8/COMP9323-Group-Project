4895fa3ac66a9e302edf01693fcc3dded3b1b68c
Jonathan-Chan8 <jonowschan@gmail.com>
2021-07-30 12:32:43 +1000

    minimal changes

--- supportsConnect/app/modules/reporting.py
+++ supportsConnect/app/modules/reporting.py
@@ -2,3 +2,12 @@
 # The following are functions related to reporting
 
 from .database import *
+
+
+# Functions to be added:
+
+# Add report 
+
+# Get report(s)
+
+# Filter by activity, support worker
